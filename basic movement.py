cab930c59857b4a9359f4eeec64ce9b2ac5effbc
MassiveTaters <105313896+MassiveTaters@users.noreply.github.com>
2022-05-12 11:59:00 -0700

    Update basic movement.py

--- basic movement.py
+++ basic movement.py
@@ -1,24 +1,28 @@
 from djitellopy import tello
 from time import sleep
 
-tello = tello.Tello()
+tello = tello.tello()
 tello.connect
 print(tello.get_battery())
 
 # takeoff command and moving forward to other side of the room #
-tello.takeoff(200)
+tello.takeoff()
+tello.move_up(120)
 tello.move_forward(500)
 
 # this is the command when the drone is on the other side of the room. it will go forward, flip, turn and head back. #
 
 tello.flip_forward()
+sleep(.5)
 tello.rotate_counter_clockwise(90)
 tello.move_forward(200)
+sleep(.5)
 tello.rotate_counter_clockwise(90)
 
 # This command is for the drone to head back to land #
 
 tello.move_forward(500)
+sleep(.5)
 tello.rotate_counter_clockwise(90)
 tello.move_forward(200)
 
